c73f027bcffce27cf818e89124ff7d3811f06999
zawadaaleksandra93 <zawadaaleksandra93@users.noreply.github.com>
2020-10-26 21:57:22 +0100

    ESA-1: bootstrap application

---
+++ src/main/java/com/projekt/event_scheduling_application/EventSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.projekt.event_scheduling_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSchedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projekt/event_scheduling_application/EventSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt.event_scheduling_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
